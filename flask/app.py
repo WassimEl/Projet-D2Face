2ebe46584e4bec949000ad166ee68f0de2c55f4c
Word.exe <welhadji@francoisdesales.be>
2024-04-22 21:00:28 +0200

    add "character"

--- flask/app.py
+++ flask/app.py
@@ -3,32 +3,40 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/login')
+@app.route("/characters", methods=["GET"])
+def characters():
+    return render_template("characters.html")
+
+
+@app.route("/login", methods=["GET"])
 def login():
-    return render_template('login.html')
+    return render_template("login.html")
 
-@app.route('/submit_login', methods=['POST'])
+
+@app.route("/submit_login", methods=["POST"])
 def submit_login():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/register', methods=['GET'])
+@app.route("/register", methods=["GET"])
 def register():
-    return render_template('register.html')
+    return render_template("register.html")
 
-@app.route('/submit_register', methods=['POST'])
+
+@app.route("/submit_register", methods=["POST"])
 def submit_register():
-    username = request.form['username']
-    email = request.form['email']
-    password = request.form['password']
-  
+    username = request.form["username"]
+    email = request.form["email"]
+    password = request.form["password"]
+
     try:
-        with open('data.json', 'r') as infile:
+        with open("data.json", "r") as infile:
             file_data = infile.read()
             if file_data.strip():
                 data = json.loads(file_data)
@@ -37,22 +45,19 @@ def submit_register():
     except FileNotFoundError:
         data = []
 
-    new_data = {
-        'username': username,
-        'email': email,
-        'password': password
-    }
+    new_data = {"username": username, "email": email, "password": password}
     data.append(new_data)
 
-    with open('data.json', 'w') as outfile:
+    with open("data.json", "w") as outfile:
         json.dump(data, outfile, indent=4)
 
-    return redirect('/result')
+    return redirect("/result")
 
-@app.route('/result', methods=['GET'])
+
+@app.route("/result", methods=["GET"])
 def result():
     try:
-        with open('data.json', 'r') as infile:
+        with open("data.json", "r") as infile:
             file_data = infile.read()
             if file_data.strip():
                 users = json.loads(file_data)
@@ -61,7 +66,8 @@ def result():
     except FileNotFoundError:
         users = []
 
-    return render_template('index.html', data=users)
+    return render_template("index.html", data=users)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
